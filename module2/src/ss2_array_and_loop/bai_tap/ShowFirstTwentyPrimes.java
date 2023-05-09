00178fd63e5ecec0b6e9345f555098f19665e011
unknown <lamtran09>
2023-05-09 20:39:00 +0700

    em nộp bài ạ

--- module2/src/ss2_array_and_loop/bai_tap/ShowFirstTwentyPrimes.java
+++ module2/src/ss2_array_and_loop/bai_tap/ShowFirstTwentyPrimes.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class ShowFirstTwentyPrimes {
     public static void main(String[] args) {
         int count = 0;
-        int n = 2;
-        int a;
+        int num = 2;
+        int length;        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập độ dài số nguyên tố mà bạn muốn in ra");
-        a = Integer.parseInt(scanner.nextLine());
-        while (count < a) {
-            if (isPrime(n)) {
-                System.out.print(n + ",");
+        length = Integer.parseInt(scanner.nextLine());
+        while (count < length) {
+            if (isPrime(num)) {
+                System.out.print(num + ",");
                 count++;
             }
-            n++;
+            num++;
         }
     }
-    public static boolean isPrime(int n) {
-        if (n < 2) {
+    public static boolean isPrime(int num) {
+        if (num < 2) {
             return false;
         } else {
-            for (int i = 2; i < n - 1; i++) {
-                if (n % i == 0) {
+            for (int i = 2; i < num - 1; i++) {
+                if (num % i == 0) {
                     return false;
                 }
             }
--- module2/src/ss2_array_and_loop/bai_tap/ShowPrimesLessThanOneHundred.java
+++ module2/src/ss2_array_and_loop/bai_tap/ShowPrimesLessThanOneHundred.java
@@ -3,12 +3,12 @@ package ss2_array_and_loop.bai_tap;
 
 public class ShowPrimesLessThanOneHundred {
     public static void main(String[] args) {
-        int n = 2;
-        while (n < 100) {
-            if (ShowFirstTwentyPrimes.isPrime(n)) {
-                System.out.print(n + ",");
+        int num = 2;
+        while (num < 100) {
+            if (ShowFirstTwentyPrimes.isPrime(num)) {
+                System.out.print(num + ",");
             }
-            n++;
+            num++;
         }
     }
 }
