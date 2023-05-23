d7fc144e9996bfbbc60eaf917ef93ea6a08beb1f
unknown <lamtran09>
2023-05-23 19:46:24 +0700

    em nộp bài ạ

--- module2/src/ss13_search_algorithm/bai_tap/string_consecutive_max/StringConsecutiveMax.java
+++ module2/src/ss13_search_algorithm/bai_tap/string_consecutive_max/StringConsecutiveMax.java
@@ -1,10 +1,33 @@
 package ss13_search_algorithm.bai_tap.string_consecutive_max;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class StringConsecutiveMax {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.print("Nhập chuỗi cần kiểm tra: ");
+        String newString = scanner.nextLine();
+        LinkedList<Character> maxString = new LinkedList<>();
+        for (int i = 0; i < newString.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(newString.charAt(i));
+            for (int j = i + 1; j < newString.length(); j++) {
+                if (newString.charAt(j) > list.getLast()) {
+                    list.add(newString.charAt(j));
+                }else {
+                    break;
+                }
+            }
+            if (list.size() > maxString.size()) {
+                maxString.clear();
+                maxString.addAll(list);
+            }
+            list.clear();
+        }
+        for (Character p : maxString) {
+            System.out.print(p);
+        }
+        System.out.println();
     }
 }
--- module2/src/ss13_search_algorithm/bai_tap/string_max/StringMax.java
+++ module2/src/ss13_search_algorithm/bai_tap/string_max/StringMax.java
@@ -6,32 +6,25 @@ import java.util.Scanner;
 public class StringMax {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập chuỗi: ");
-        String string = scanner.nextLine();
-        LinkedList<Character> max = new LinkedList<>();
-        // Tìm chuỗi tăng dần có độ dài lớn nhất
-        for (int i = 0; i < string.length(); i++) { //Vòng lặp bên ngoài
-            //Khai báo một linked list trung gian các kí tự của chuỗi tăng dần
+        System.out.print("Nhập chuỗi cần kiểm tra: ");
+        String newString = scanner.nextLine();
+        LinkedList<Character> maxString = new LinkedList<>();
+        for (int i = 0; i < newString.length(); i++) {
             LinkedList<Character> list = new LinkedList<>();
-            //thêm kí tự thứ i vào trong list
-            list.add(string.charAt(i));
-            for (int j = i + 1; j < string.length(); j++) { //vòng lặp bên trong
-                //Kiểm tra kí tự tiếp theo có lớn hơn kí tự cuối cùng trong list hay không
-                if (string.charAt(j) > list.getLast()) {
-                    //Nếu có thì thêm vào trong list
-                    list.add(string.charAt(j));
+            list.add(newString.charAt(i));
+            for (int j = i + 1; j < newString.length(); j++) {
+                if (newString.charAt(j) > list.getLast()) {
+                    list.add(newString.charAt(j));
                 }
             }
-            //So sánh kích cỡ của list trung gian và list chứa chuỗi tăng dần có kích thước lớn nhất
-            if (list.size() > max.size()) {
-                //Nếu kích cỡ của list trung gian lớn hơn kích cỡ của list có độ dài lớn nhất thì gán lại max
-                max.clear();
-                max.addAll(list);
+            if (list.size() > maxString.size()) {
+                maxString.clear();
+                maxString.addAll(list);
             }
             list.clear();
         }
-        for (Character ch : max) { // 1 vòng lặp
-            System.out.print(ch); // 1 câu lệnh
+        for (Character p : maxString) {
+            System.out.print(p);
         }
         System.out.println();
     }
