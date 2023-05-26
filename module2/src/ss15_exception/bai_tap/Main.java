5ed4769dde06d7d7f3215c7b2f4f5afaae3c2482
unknown <lamtran09>
2023-05-27 00:50:16 +0700

    em nộp bài ạ

--- module2/src/ss15_exception/bai_tap/Main.java
+++ module2/src/ss15_exception/bai_tap/Main.java
@@ -3,35 +3,72 @@ package ss15_exception.bai_tap;
 import java.util.Scanner;
 
 public class Main {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        boolean flag = true;
-//        System.out.print("Nhập giá trị cạnh a: ");
-//        try {
-//            int edgeA = Integer.parseInt(scanner.nextLine());
-//            flag = false;
-//        }catch (NumberFormatException numberFormatException){
-//            System.out.println("Bạn nhập ");
-//        }
-//        flag = true;
-//
-//        System.out.print("Nhập giá trị cạnh b: ");
-//        int edgeB = Integer.parseInt(scanner.nextLine());
-//        System.out.print("Nhập giá trị cạnh c: ");
-//        int edgeC = Integer.parseInt(scanner.nextLine());
-//        try {
-//            triangle(edgeA, edgeB, edgeC);
-//        } catch (IllegalTriangleException triangleException) {
-//            System.out.println(triangleException.getMessage());
-//            System.out.println("Nhập lại");
-//        }
-//    }
-//
-//    public static boolean triangle(int edgeA, int edgeB, int edgeC) throws IllegalTriangleException {
-//        if (edgeA < 0 || edgeB < 0 || edgeC < 0 || edgeA + edgeB <= edgeC || edgeA + edgeC <= edgeB || edgeB + edgeC <= edgeA) {
-//            throw new IllegalTriangleException("Không hợp lệ, Không phải tam giác");
-//        }
-//        return true;
-//    }
-//}
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int edgeA;
+        int edgeB;
+        int edgeC;
+
+        while (true) {
+            do {
+                System.out.print("Nhập giá trị cạnh a: ");
+                try {
+                    edgeA = Integer.parseInt(scanner.nextLine());
+                    if (edgeA <= 0) {
+                        throw new IllegalTriangleException("Nhập sai nhập lại");
+                    }
+                    break;
+                } catch (IllegalTriangleException e) {
+                    System.out.println(e.getMessage());
+                } catch (NumberFormatException e) {
+                    System.out.println("Nhập sai format");
+                }
+            } while (true);
+            do {
+                System.out.print("Nhập giá trị cạnh b: ");
+                try {
+                    edgeB = Integer.parseInt(scanner.nextLine());
+                    if (edgeB <= 0) {
+                        throw new IllegalTriangleException("Nhập sai nhập lại");
+                    }
+                    break;
+                } catch (IllegalTriangleException e) {
+                    System.out.println(e.getMessage());
+                } catch (NumberFormatException e) {
+                    System.out.println("Nhập sai Format");
+                }
+            } while (true);
+            do {
+                System.out.print("Nhập giá trị cạnh c: ");
+                try {
+                    edgeC = Integer.parseInt(scanner.nextLine());
+                    if (edgeC <= 0) {
+                        throw new IllegalTriangleException("Nhập sai nhập lại");
+                    }
+                    break;
+                } catch (IllegalTriangleException e) {
+                    System.out.println(e.getMessage());
+                } catch (NumberFormatException e) {
+                    System.out.println("Nhập sai Format");
+                }
+            } while (true);
+
+            try {
+                triangle(edgeA, edgeB, edgeC);
+                System.out.println("Đây là một tam giác");
+                break;
+            } catch (IllegalTriangleException triangleException) {
+                System.out.println(triangleException.getMessage());
+                System.out.println("Nhập sai nhập lại");
+            }
+        }
+    }
+
+    public static boolean triangle(int edgeA, int edgeB, int edgeC) throws IllegalTriangleException {
+        if (edgeA + edgeB <= edgeC || edgeA + edgeC <= edgeB || edgeB + edgeC <= edgeA) {
+            throw new IllegalTriangleException("Không hợp lệ, Không phải tam giác");
+        }
+        return true;
+    }
 }
