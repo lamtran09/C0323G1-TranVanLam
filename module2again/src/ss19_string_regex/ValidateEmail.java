d4603d554201a90cdd0fb9e2454de379069202ed
unknown <lamtran09>
2023-06-28 09:12:25 +0700

    em nộp bài ạ

--- module2again/src/ss19_string_regex/ValidateEmail.java
+++ module2again/src/ss19_string_regex/ValidateEmail.java
@@ -1,6 +1,6 @@
 package ss19_string_regex;
     public class ValidateEmail {
-        private static final String VALIDATE_REGEX_EMAIL_CUSTOMER = "^[A-Za-z0-9][A-Za-z0-9]{0,32}@[A-Za-z0-9]{2,12}(\\.[A-Za-z0-9]{2,12})$";
+        private static final String VALIDATE_REGEX_EMAIL_CUSTOMER = "^[a-zA-Z0-9]\\w{5,31}@[a-z]{1,11}\\.[a-z]{1,11}(\\.[a-z]{1,11})?$";
 
         public static boolean checkRegexEmailCustomer(String emailCustomer) {
             return emailCustomer.matches(VALIDATE_REGEX_EMAIL_CUSTOMER) ;
